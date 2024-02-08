92da143fefdfe2bf944806ce135cbc53c8af6302
Sakibur Rahman <sakib.sr20@gmail.com>
2024-02-08 06:13:16 -0800

    Update uni2bn_test.py

--- uni2bn_test.py
+++ uni2bn_test.py
@@ -2,9 +2,9 @@ import uni2bn
 
 def test_uni2bn():
     # Test conversion from Unicode Bangla text to Bangla characters
-    unicode_text = '\u0986\u09AE\u09BF \u09B8\u09BE\u0995\u09BF\u09AC\u09C7\u09B0 \u09A6\u09AC\u09BE\u09B0\u09BE \u09A4\u09C8\u09B0\u09BF\u0964'
+    unicode_text = '\u09A1\u09C7\u09AD\u09C7\u09B2\u09AA\u09BE\u09B0 \u09B8\u09BE\u0995\u09BF\u09AC'
     bangla_text = uni2bn.uni2bn(unicode_text)
-    expected_result = 'আমি সাকিবের দ্বারা তৈরি।'
+    expected_result = 'ডেভেলপার সাকিব'
     # Convert both texts to Unicode for proper comparison
     assert bangla_text == expected_result, f"Expected: {expected_result.encode('unicode_escape')}, Actual: {bangla_text.encode('unicode_escape')}"
 
